ace11060b087f8195c570e6657522e18d5f39061
Jonas Andersson <jonas.andersson.94@hotmail.com>
2016-04-15 00:57:48 +0200

    Added angular ui router for handling routes

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,27 @@
-var trackworkApp = angular.module('TrackWorkApp', []);
+var trackworkApp = angular.module('TrackWorkApp', ['ui.router']);
 
-trackworkApp.controller('HomeCtrl', function ($scope) {
-  this.message = 'Home';
+trackworkApp.controller('IndexController', function ($scope) {
+  this.message = 'Homeawdwadadadadad';
+
+  console.log("wdawd");
+
+});
+
+trackworkApp.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/404");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('index', {
+      url: "/",
+      templateUrl: "partials/index.html",
+      controller: "IndexController",
+      controllerAs: "v"
+    })
+    .state('404', {
+      url: '/404',
+      template: "<h1>404 - Not found</h1>"
+    });
 });
