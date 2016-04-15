b98845544226128e1ceebdda91e12d91609fe398
Jonas Andersson <jonas.andersson.94@hotmail.com>
2016-04-15 16:45:43 +0200

    Added content to profile

--- scripts/controllers/profile.controller.js
+++ scripts/controllers/profile.controller.js
@@ -9,5 +9,13 @@ angular
 function ProfileController() {
   var vm = this;
 
-  vm.message = "Hi, I'm profile";
+  vm.name = "Jonas Andersson";
+  vm.birthday = "12-02-1984";
+  vm.email = "jonas@jonasanderssonweb.se";
+  vm.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet massa ac molestie accumsan. Nulla velit purus, aliquet nec felis nec, pulvinar placerat est. Vivamus dapibus sapien eu nunc bibendum laoreet. Fusce molestie erat sapien, id scelerisque urna maximus ut.";
+
+  vm.save = function() {
+    alert("Your profile has been saved!");
+  };
+
 }
