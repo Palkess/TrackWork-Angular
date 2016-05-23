cd3371e310758d285d765ff24523c507250f6079
Jonas Andersson <jonas.andersson.94@hotmail.com>
2016-05-23 15:10:03 +0200

    Connected controller to UserService

--- public/scripts/controllers/profile.controller.js
+++ public/scripts/controllers/profile.controller.js
@@ -4,15 +4,14 @@
  */
 angular
     .module('TrackWorkApp')
-    .controller('ProfileController', ProfileController);
+    .controller('ProfileController', ['UserService', ProfileController]);
 
-function ProfileController() {
+function ProfileController(UserService) {
   var vm = this;
+  vm.user = UserService.user();
 
-  vm.name = "Jonas Andersson";
-  vm.birthday = "12-02-1984";
-  vm.email = "jonas@jonasanderssonweb.se";
-  vm.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet massa ac molestie accumsan. Nulla velit purus, aliquet nec felis nec, pulvinar placerat est. Vivamus dapibus sapien eu nunc bibendum laoreet. Fusce molestie erat sapien, id scelerisque urna maximus ut.";
+  vm.user.birthday = "12-02-1984";
+  vm.user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet massa ac molestie accumsan. Nulla velit purus, aliquet nec felis nec, pulvinar placerat est. Vivamus dapibus sapien eu nunc bibendum laoreet. Fusce molestie erat sapien, id scelerisque urna maximus ut.";
 
   vm.save = function() {
     alert("Your profile has been saved!");
