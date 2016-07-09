684df2afbb27826169c68be1f104cbff12a268a1
Jonas Andersson <jonas.andersson.94@hotmail.com>
2016-07-09 11:18:36 +0200

    Added model for entry against the database

---
+++ server/models/entry.model.js
@@ -0,0 +1,15 @@
+// Model for Entry
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Entry = new Schema({
+  'ownerID': String,
+  'description': String,
+  'start': Date,
+  'end': Date,
+  'overtime': Boolean,
+  'holiday': Boolean,
+  'created': { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('entries', Entry);
