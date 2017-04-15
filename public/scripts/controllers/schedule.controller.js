d0c28b39fb1cca7cbd5499c5e112ea150004d21f
Jonas <jonas.andersson.web@gmail.com>
2017-04-15 14:54:10 +0200

    Added promise-handling and removed testdata

--- public/scripts/controllers/schedule.controller.js
+++ public/scripts/controllers/schedule.controller.js
@@ -9,38 +9,6 @@ angular
 function ScheduleController($sessionStorage, EntryService)   {
   var vm = this;
 
-  // Set this to vm.entries for example data
-  var exampleEntries = {
-    '2016-04-14': [
-      {
-        'description': 'Did some stuff',
-        'start': '2016-04-14T12:00Z',
-        'end': '2016-04-14T14:00Z',
-        'overtime': true
-      },
-      {
-        'description': 'Did some more stuff',
-        'start': '2016-04-14T16:00Z',
-        'end': '2016-04-14T19:00Z',
-        'overtime': false
-      }
-    ],
-    '2016-04-13': [
-      {
-        'description': 'Did some stuff',
-        'start': '2016-04-13T12:00Z',
-        'end': '2016-04-13T14:00Z',
-        'overtime': true
-      },
-      {
-        'description': 'Did some more stuff',
-        'start': '2016-04-13T16:00Z',
-        'end': '2016-04-13T19:00Z',
-        'overtime': false
-      }
-    ]
-  };
-
   /**
    * Retrieves the entries from database and puts them into the view
    *
@@ -93,9 +61,12 @@ function ScheduleController($sessionStorage, EntryService)   {
       'end': vm.end,
       'overtime': vm.overtime ? vm.overtime : false,
       'holiday': false
+    })
+    .then(function(data) {
+      fetchEntries();
+    }, function(err) {
+      console.log(err);
     });
-
-    fetchEntries();
 
     vm.description = '';
     vm.start = '';
